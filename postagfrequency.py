c0fbd2a0383fcaf161b3fe6a0d3a894791496557
avlmg <148965851+avlmg@users.noreply.github.com>
2025-06-26 01:51:11 +0200

    # added stopwords and punctuation removal to get cleaner graphics

--- postagfrequency.py
+++ postagfrequency.py
@@ -4,8 +4,13 @@ import os
 import xml.etree.ElementTree as ET
 from collections import Counter, defaultdict
 import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords # nltk stopwords
+import spacy
+spacy.cli.download("en_core_web_sm") # model for stopwords from spacy
+import unicodedata # import for sorting out punctuation marks
 
-synaf_dir = "/Users/kanakpandit/Desktop/TextTechnology/synaf_xml"
+synaf_dir = "./synaf_xml"
 files = [f for f in os.listdir(synaf_dir) if f.endswith(".xml")]
 
 # Data holders
@@ -43,10 +48,38 @@ for filename in files:
         if dep:
             year_dep_counts[year][dep] += 1
 
+####################### ELIMINATE STOPWORDS AND PUNCTUATION #############################
+
+shakespeare_stopwords = {
+    "thou", "thee", "thy", "thine", "ye", "hast", "hath", "dost", "doth",
+    "art", "wilt", "shalt", "wouldst", "couldst", "shouldst", "didst",
+    "ere", "wherefore", "whence", "hence", "thence", "oft", "nay", "yea",
+    "'tis", "'twas", "o’er", "e’en", "ne’er", "i’", "th’", "methinks", "perchance", "prithee", "marry", "fain", "anon", "lo"
+}
+
+nlp = spacy.load("en_core_web_sm")
+spacy_stops = nlp.Defaults.stop_words
+nltk.download("stopwords")
+nltk_stops = set(stopwords.words("english"))
+combined_stopwords = spacy_stops.union(nltk_stops).union(shakespeare_stopwords)
+
+# check for punctuation marks
+def is_punctuation(token):
+    return all(unicodedata.category(char).startswith('P') for char in token)
+
+
 # Example: print top 10 lemmas for a sample year
 sample_year = "1595"
 print(f"Top lemmas in {sample_year}:")
-for lemma, count in year_lemma_counts[sample_year].most_common(10):
+
+# Filtered list of non-stopword non-punctuation lemmas
+filtered_lemmas = [
+    (lemma, count)
+    for lemma, count in year_lemma_counts[sample_year].most_common()
+    if lemma not in combined_stopwords and not is_punctuation(lemma)
+]
+
+for lemma, count in filtered_lemmas[:10]:
     print(f"  {lemma}: {count}")
 
 # Plot example: POS tag distribution over years for a few POS tags
